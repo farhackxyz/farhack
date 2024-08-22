0d343780c8b2e2bb0e1862de7243672a1435b195
dylsteck <dylancsteck@gmail.com>
2024-08-21 23:20:56 -0400

    fix: ensure no type mismatches for vercel builds

--- kysely.ts
+++ kysely.ts
@@ -98,13 +98,15 @@ export const addTicket = async (
   userId: number,
   userAddress: string,
   hackathonId: number,
-  txnHash: string
+  txnHash: string,
+  ticketType: 'priority' | 'day'
 ) => {
   return await db.insertInto('tickets').values({
     user_id: userId,
     user_address: userAddress,
     hackathon_id: hackathonId,
     txn_hash: txnHash,
+    ticket_type: ticketType
   }).returningAll().executeTakeFirst();
 };
 

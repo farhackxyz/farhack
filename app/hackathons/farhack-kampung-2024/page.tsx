dc71238860149278f89395f32eda703712bd662d
dylsteck <dylancsteck@gmail.com>
2024-09-03 11:44:48 -0400

    wip: cleaner and new details

--- app/hackathons/farhack-kampung-2024/page.tsx
+++ app/hackathons/farhack-kampung-2024/page.tsx
@@ -38,18 +38,6 @@ export default async function FarhackKampung2024Page() {
     const startDate = new Date(hackathon.start_date).toLocaleDateString(undefined, { year: 'numeric', month: 'long', day: 'numeric' });
     const endDate = new Date(hackathon.end_date).toLocaleDateString(undefined, { year: 'numeric', month: 'long', day: 'numeric' });
 
-    const schedule = [
-        { date: '9/15', info: 'Daytime: Hackers and partners arrive at the village\n6pm - 8pm: Community dinner\n8pm - 12am: Opening party' },
-        { date: '9/16', info: '10am - 12pm: Intro and speakers\n12pm - 1pm: Outdoor lunch\n1pm - 4pm: Workshop sessions\n4pm - night: Free working time' },
-        { date: '9/17', info: '10am - 11am: State of Farcaster by Dylan\n11am - 12pm: Hackathon kickoff\n12pm - 1pm: Lunch\n1pm - 6pm: Team creation + hacking\n6pm - 10pm: Hackathon happy hour' },
-        { date: '9/18', info: '10am - 12pm: Workshops\n12pm - 1pm: Lunch\n1pm - 6pm: Hacking time' },
-        { date: '9/19', info: '10am - 12pm: Workshops\n12pm - 1pm: Lunch\n1pm - 11:59pm: Submissions due' },
-        { date: '9/20', info: '9am - 12pm: Award ceremony\n12pm - 6pm: Day 1 of ETHGlobal' },
-        { date: '9/21', info: '11am - 8pm: Explore Singapore\n8pm - 12am: Dinner and hang out' },
-        { date: '9/22', info: '11am - 8pm: Explore Singapore\n8pm - 12am: Final night party' },
-        { date: '9/23', info: '10am - 12pm: Hackers and partners leave the village' },
-    ];
-
     return (
         <div className="pt-[10%] md:pt-[5%]">
             <div className="text-white flex flex-col gap-1 items-start pl-[7.5%] md:pl-[4.5%]">
@@ -57,45 +45,50 @@ export default async function FarhackKampung2024Page() {
                     <div className="flex flex-col mt-7 md:mt-0 mb-10 md:mb-0 md:w-1/2 gap-2 mr-[15%]">
                         <div className="flex flex-col gap-1 items-start">
                             <p className="text-4xl font-medium">
-                                {hackathon.name}
+                                Kampung
                             </p>
                             <p>
                                 {startDate} - {endDate}
                             </p>
                         </div>
-                        <p className="mt-2 text-xl font-normal text-white/95">
+                        <p className="mt-2 text-xl font-normal text-white/95 max-w-[60%]">
                             {hackathon.description}
                         </p>
-                        <BuyTicketsModal user={user} hasTicket={userHasPriorityTicket} ticketsLeft={priorityTicketsLeft} ticketType='priority' />
-                        {!userHasPriorityTicket && <BuyTicketsModal user={user} hasTicket={userHasDayTicket} ticketsLeft={dayTicketsLeft} ticketType='day' />}
-                        <div className="mt-4 max-w-[75%]">
-                            <div className="flex flex-row gap-2 items-center mb-1.5">
-                                <CalendarIcon className="w-4" />
-                                <p className="text-lg font-medium">Schedule</p>
+                        <div className="mt-10 max-w-[75%]">
+                            <div className="flex flex-row gap-2 items-center mb-2.5">
+                                <p className="text-xl font-medium">
+                                    Kampung Priority Pass: $750
+                                </p>
                             </div>
-                            <div className="flex overflow-x-auto gap-4 scrollbar-hide max-w-full">
-                                {schedule.map((item, index) => (
-                                    <div
-                                        key={index}
-                                        className="min-w-[250px] flex-shrink-0 bg-[#3a3a3a] p-4 rounded-lg"
-                                    >
-                                        <p className="text-lg font-medium">{item.date}</p>
-                                        <p className="mt-2 whitespace-pre-wrap">{item.info}</p>
-                                    </div>
-                                ))}
+                            <div className="flex flex-row gap-2 items-start text-lg max-w-auto md:max-w-[75%]">
+                                <p>
+                                    <b>Stay at: </b><br/>
+                                    -Luxury Villa at Singapore National Resort and Golf Course  <br/>
+                                    -Full Kitchen, Pool access and more  <br/>
+                                    -8 nights and 9 days  <br/>
+                                </p>
+                                <p>
+                                    <b>Access to: </b><br/>
+                                    -FarHack  <br/>
+                                    -ETHGlobal  <br/>
+                                    -Ethereum Singapore  <br/>
+                                </p>
                             </div>
+                            <BuyTicketsModal user={user} hasTicket={userHasPriorityTicket} ticketsLeft={priorityTicketsLeft} ticketType='priority' />
                         </div>
-                        <div className="mt-4 max-w-[75%]">
-                            <div className="flex flex-row gap-2 items-center mb-1.5">
-                                <DocumentTextIcon className="w-4" />
-                                <p className="text-lg font-medium">Ticket Details</p>
+                        <div className="mt-7 max-w-[75%]">
+                            <div className="flex flex-row gap-2 items-center mb-2.5">
+                                <p className="text-xl font-medium">
+                                FarHack Day Pass: $20
+                                </p>
                             </div>
-                            <p className="text-lg">
-                            All prices are in USD and are per person. Priority tickets include full stay and the most comfortable accommodation at our luxurious village in Singapore. There is also a local pass available for those who do not need accommodation but would like to participate in the Farcaster Hackathon.
+                            <p className="text-lg mb-5">
+                                Access to FarHack
                             </p>
+                            {!userHasPriorityTicket ? <BuyTicketsModal user={user} hasTicket={userHasDayTicket} ticketsLeft={dayTicketsLeft} ticketType='day' /> : <></>}
                         </div>
                     </div>
-                    <div className="flex flex-col md:w-1/2 gap-2">
+                    <div className="hidden md:flex flex-col md:w-1/2 gap-2">
                         <img src={hackathon.square_image} alt={`${hackathon?.name} Hackathon`} loading="lazy" className="rounded-lg w-[75%]" />
                     </div>
                 </div>
